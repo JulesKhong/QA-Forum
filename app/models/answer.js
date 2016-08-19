45940e2ee7413e573b9396280049e5d189fa76e0
Julia Dickey <julia.dickey@gmail.com>
2016-08-19 13:29:17 -0700

    Resolve error in displaying answers

--- app/models/answer.js
+++ app/models/answer.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   author: DS.attr(),
-  content: DS.belongsTo('question', {async: true})
+  content: DS.attr(),
+  question: DS.belongsTo('question', {async: true})
 });
